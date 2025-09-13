87c2801c36d09a0e5d79cbad8caef39682cca7ab
Amaan1787 <sayedamaan03@gmail.com>
2025-09-13 20:24:09 +0530

    Home & main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Hero from "./../components/Hero";
+import Carousel from "./../components/Carousel";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
-      Home
+      <Carousel />
     </div>
   );
 };
